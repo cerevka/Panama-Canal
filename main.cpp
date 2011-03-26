ebf927148d9fed2a137ea4c84c6d401afe704968
Tomáš Čerevka <tomas.cerevka@gmail.com>
2011-03-26 14:11:46 +0100

    Inicializace.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: tom
+ *
+ * Created on March 26, 2011, 2:10 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,24 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux-x86
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux-x86
+CND_ARTIFACT_NAME_Debug=panamacanal
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux-x86/panamacanal
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Debug=panamacanal.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux-x86/package/panamacanal.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux-x86
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux-x86
+CND_ARTIFACT_NAME_Release=panamacanal
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux-x86/panamacanal
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Release=panamacanal.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux-x86/package/panamacanal.tar
